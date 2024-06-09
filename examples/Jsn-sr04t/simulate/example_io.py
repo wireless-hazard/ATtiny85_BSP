c1bcb35dcd82e4e683581fb0a4813906440729e8
wireless-hazard <magnocostamaia@gmail.com>
2024-06-09 20:24:09 -0300

    Add empirical conversion between counter variable and up time for JSN-SR04T

--- examples/Jsn-sr04t/simulate/example_io.py
+++ examples/Jsn-sr04t/simulate/example_io.py
@@ -18,7 +18,7 @@ class XPin(pysimulavr.Pin):
     
   def SetInState(self, pin):
     pysimulavr.Pin.SetInState(self, pin)
-    print(f"{self.name}='{pin.toChar()}' (t={sim.getCurrentTime()}ns)")
+    #print(f"{self.name}='{pin.toChar()}' (t={sim.getCurrentTime()}ns)")
 
 if __name__ == "__main__":
 
@@ -32,16 +32,23 @@ if __name__ == "__main__":
   sim.setVCDDump("result.vcd", sigs)
   
   a7 = XPin(dev, "B2", "L")
+
+  uptime_string = (296750, 607000,908438,1152000, 1413000, 1648800)
   
   sim.dmanStart()
-  
-  sim.doRun(sim.getCurrentTime() + 15000000)
-  a7.SetPin("H")
-  sim.doRun(sim.getCurrentTime() + 50000)
-  a7.SetPin("L")
-  sim.doRun(sim.getCurrentTime() + 200000)
-  distancia = sim.getWordByName(dev, "distance")
-  print(f"Valor retornado por ReadDistance: {distancia}")
+  for uptime in uptime_string:
+    sim.doRun(sim.getCurrentTime() + 15000000)
+    a7.SetPin("H")
+    sim.doRun(sim.getCurrentTime() + uptime) # 1 ms
+    a7.SetPin("L")
+    sim.doRun(sim.getCurrentTime() + 200000)
+    counter = sim.getWordByName(dev, "distance")
+    print(f"Counter: {counter}")
+    #print(f"Rate between time on and count: {uptime/counter}")
+    uptime_calc_ns = (434*counter)
+    #distance = (uptime_calc_ns*340)/(2*(1000000000))
+    distance = sim.getWordByName(dev, "actual_distance")
+    print(f"Distance: {distance}")
   
   sim.dmanStop()  
 
