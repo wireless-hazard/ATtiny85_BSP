aa7cb796f3f000eb4759d1dc989a5b63c2971a7c
Magno Maia <magnocostamaia@gmail.com>
2024-07-18 22:54:38 -0300

    Add new special function register definitions

--- src/at85_sfr_defines.hpp
+++ src/at85_sfr_defines.hpp
@@ -34,5 +34,54 @@ static inline constexpr uint8_t USIWM_MSK     = 0b00110000U;
 static inline constexpr uint8_t USICS_MSK     = 0b00001100U;
 static inline constexpr uint8_t ALL_USICS_MSK = 0b00001110U;
 
+typedef union
+{
+    uint8_t reg;
+    struct 
+    {   
+        uint8_t USICNT_ : 4; // Counter Value
+        uint8_t USIDC_  : 1; // Data Output Collision
+        uint8_t USIPF_  : 1; // Stop Condition Flag
+        uint8_t USIOIF_ : 1; // Counter Overflow Interrupt Flag
+        uint8_t USISIF_ : 1; // Start Condition Interrupt Flag
+    } bit;
+} USISR_t;
+
+static volatile USISR_t *USISR_reg = (volatile USISR_t *)(0x0E + 0x20);
+static_assert(sizeof(USISR_t) == sizeof(uint8_t));
+
+typedef union
+{
+    uint8_t reg;
+    struct
+    {
+        uint8_t USITC_  : 1; // Toggle Clock Port Pin
+        uint8_t USICLK_ : 1; // Clock Strobe
+        uint8_t USICS_  : 2; // Clock Source Select
+        uint8_t USIWM_  : 2; // Wire Mode
+        uint8_t USIOIE_ : 1; // Counter Overflow Interrupt Enable
+        uint8_t USISIE_ : 1; // Start Condition Interrupt Enable
+    } bit;
+} USICR_t;
+
+static volatile USICR_t *USICR_reg = (volatile USICR_t *)(0x0D + 0x20);
+static_assert(sizeof(USICR_t)==sizeof(uint8_t));
+
+typedef union
+{
+    uint8_t reg;
+    struct
+    {
+        uint8_t PORTB0_  : 1;
+        uint8_t PORTB1_  : 1;
+        uint8_t PORTB2_  : 1;
+        uint8_t PORTB3_  : 1;
+        uint8_t PORTB4_  : 1;
+        uint8_t PORTB5_  : 1;
+        uint8_t reserved : 2;
+    } bit;
+} PORTB_t;
+static volatile PORTB_t *PORTB_reg = (volatile PORTB_t *)(0x18 + 0x20);
+static_assert(sizeof(PORTB_t)==sizeof(uint8_t));
 
 #endif /*AT85_SFR_DEF_H_*/
